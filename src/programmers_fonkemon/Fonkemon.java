a81533e8200b92c623ff12d024394405854f11fa
Lee, Gwanghyeon <cupid0919@gmail.com>
2025-03-16 17:45:57 +0900

    최종 코드

--- src/programmers_fonkemon/Fonkemon.java
+++ src/programmers_fonkemon/Fonkemon.java
@@ -23,13 +23,14 @@ public class Fonkemon {
         int result = Math.min(uniqueMons.size(), maxSelect);
         System.out.println("선택할 수 잇는 최대 종류의 폰켓몬 개수: " + result);
         
-        return uniqueMons.size(); // 반환값 변경
+        return result; // 반환값 변경
     }
-
+ // 실행 테스트를 위해서 필요한 부분
+/*
     public static void main(String[] args) {
         Fonkemon sol = new Fonkemon(); // Fonkemon 객체 생성
         int[] nums = {3, 1, 2, 3}; // 테스트 데이터
         int result = sol.solution(nums); // solution 메서드 실행
         System.out.println("결과: " + result);
-    }
+    } */
 }
